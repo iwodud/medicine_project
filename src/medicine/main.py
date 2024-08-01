bd1e4474329e444e56573f22875e44a9ba20652e
iwodud <iwo.dudkiewicz@gmail.com>
2024-08-01 07:15:27 +0200

    remove how_much_in_mg() function and clean main

--- src/medicine/main.py
+++ src/medicine/main.py
@@ -3,18 +3,6 @@ from typing import List
 from medicine import Medicine
 
 
-def how_much_in_mg(*medicines):
-    """Takes medicine's names (STR) and returns amount of the medicine in milligrams"""
-    sum_of_mg = []  # to this list I add milligrams multiplied by the number of pills
-    for medicine in medicines:
-        assert isinstance(medicine, Medicine), 'medicines is supposed to be medicine_classes.Medicine'
-        amount_of_pills = medicine.current_amount
-        mg_per_pill = medicine.dose
-        amount_of_mg = amount_of_pills * mg_per_pill
-        sum_of_mg.append(amount_of_mg)
-    return sum(sum_of_mg)
-
-
 paracetamol_500 = Medicine('paracetamol', 500, 1500, 100)
 paracetamol_750 = Medicine('paracetamol', 750, 1500, 100)
 ibuprofen_600 = Medicine('ibuprofen', 600, 1200, 40)
--- src/medicine/medicine.py
+++ src/medicine/medicine.py
@@ -8,11 +8,11 @@ class Medicine:
         name (str): name of medicine with it's dose per pill (for instance apap_600)
         dose (int, float): dose of one pill (for apap_600 it's 600)
         daily_dose (int, float): dose of medicine you take per day
-        current_amount (int, float): amount of pills you have at the moment
+        initial_amount (int, float): amount of pills you have at the moment
 
     Methods:
-        __init__(self, name, dose, daily_dose, current_amount): initializes arguments
-        current_mg(self): basing on self.current_amount, the method returns amount of the medicine in milligrams.
+        __init__(self, name, dose, daily_dose, initial_amount): initializes arguments
+        initial_mg(self): basing on self.initial_amount, the method returns amount of the medicine in milligrams.
         __str__(self): describes instance nicely
         __repr__(self): describes instance concretely
         
@@ -23,7 +23,7 @@ class Medicine:
     """
     instances = {}  # dictionary before __init__ is shared by all instances
 
-    def __init__(self, name: str, dose: int | float, daily_dose: int | float, current_amount: int | float):
+    def __init__(self, name: str, dose: int | float, daily_dose: int | float, initial_amount: int | float):
         if not isinstance(name, str):
             raise ValueError('name is supposed to be a string')
         self.name = name
@@ -40,11 +40,11 @@ class Medicine:
             raise ValueError('Negative doses do not exist')
         self.daily_dose = daily_dose
 
-        if not isinstance(current_amount, (int, float)):
-            raise ValueError('current_amount is supposed to be a number')
-        if current_amount < 0:
+        if not isinstance(initial_amount, (int, float)):
+            raise ValueError('initial_amount is supposed to be a number')
+        if initial_amount < 0:
             raise ValueError('It is not possible to have negative amounts of pills')
-        self.current_amount = current_amount
+        self.initial_amount = initial_amount
         self.__add_medicine_to_dict(self.instances)
         self.__append_instance_to_file()
     
@@ -52,8 +52,8 @@ class Medicine:
     def __add_medicine_to_dict(self, dict_name):
         """Adds name_dose of an instance (self.name + _ + self.dose), with dictionary containing it's parameters as value, to dictionary with a given name (dict_name)"""
         assert isinstance(dict_name, dict), 'dict_name is supposed to be DICT'
-        dict_name[str(self.name) + '_' + str(self.dose)] = {'daily_dose': self.daily_dose, 'amount_of_mg': self.current_mg(), 
-                                                            'amount_of_pills': self.current_amount, 'date': str(dt.date.today())}
+        dict_name[str(self.name) + '_' + str(self.dose)] = {'daily_dose': self.daily_dose, 'amount_of_mg': self.initial_mg(), 
+                                            'amount_of_pills': self.initial_amount, 'date': str(dt.date.today())}
 
 
     def __append_instance_to_file(self, file_name='data.json'):
@@ -71,17 +71,17 @@ class Medicine:
             json.dump(instances_dict, file, indent=4)
 
 
-    def current_mg(self):
-        """Basing on self.current_amount returns amount of the medicine in milligrams at the moment of creating the medicine"""
-        amount_of_pills = self.current_amount
+    def initial_mg(self):
+        """Basing on self.initial_amount returns amount of the medicine in milligrams at the moment of creating the medicine"""
+        amount_of_pills = self.initial_amount
         mg_per_pill = self.dose
         amount_of_mg = amount_of_pills * mg_per_pill
         return amount_of_mg
 
 
     def __str__(self):
-        return f'{self.name.title()} medicine with dose {self.dose} mg per pill (daily dose {self.daily_dose} mg) has {self.current_amount} pills left.'
+        return f'{self.name.title()} medicine with dose {self.dose} mg per pill (daily dose {self.daily_dose} mg) has {self.initial_amount} pills left.'
 
 
     def __repr__(self):
-        return f'Medicine({self.name}, {self.dose}, {self.daily_dose}, {self.current_amount})'
+        return f'Medicine({self.name}, {self.dose}, {self.daily_dose}, {self.initial_amount})'
