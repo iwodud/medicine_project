97bb17600a085ad2e32f858139c943bbea92c64f
iwodud <iwo.dudkiewicz@gmail.com>
2024-08-03 14:04:03 +0200

    add sceleton of user functions

--- notes.py
+++ notes.py
@@ -23,6 +23,3 @@ birthday = datetime.date(2024, 5, 29)
 
 till_bday = birthday - today
 print(till_bday)
-
-# rzeczy potrzebne do obliczenia ilości pozostałych dni, ilości pozostałych tabletek, daty kiedy tabletki się wyczerpią: 
-# data utworzenia instancji leku, ilość mg leku w tablietce, pozostałą ilość mg leku
--- src/medicine/functions_user.py
+++ src/medicine/functions_user.py
@@ -15,7 +15,7 @@ def add_pills(medicine_name: str, amount_of_pills: int, file_name='data.json'):
     export_to_file(instances_dict)
 
 
-def remove_instance_from_file(key, file_name='data.json'):
+def remove_medicine_from_file(key, file_name='data.json'):
     """Removes chosen medicine basing on name (key) from dictionary in file. By default it removes itself from data.json"""
     if not isinstance(file_name, str):
         raise TypeError('file_name must be a string')
@@ -31,3 +31,11 @@ def remove_instance_from_file(key, file_name='data.json'):
     instances_dict = import_from_file()
     remove_medicine_from_dict(instances_dict, key)
     export_to_file(instances_dict)
+
+
+def change_daily_dose():
+    pass
+
+
+def show_medicine():
+    pass
